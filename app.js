0af05d411472aa09e2e2da722bce524402561d4f
tomaspedl <tomaspeld@gmail.com>
2019-11-19 15:37:27 -0300

    tp express

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
-
-app.get("/", function(req, res){
-    res.send("Bienvenidos")
-})
+const heroes = require("./heroes.json");
 
 app.listen(3000,function(){
-console.log("http://localhost:3000")
-})
+console.log("http://localhost:3000");
+});
+
+app.get("/", function(req, res){
+    res.send("Ni Superman, Iron Man o La Mujer Maravilla son tan importantes cómo las y los Heroes de carne y hueso que encontrarás en este sitio. Esperamos que ellas y ellos te sirvan como inspiración para poder cumplir tus objetivos. Recuerda: ¡nunca pares de creer en ti!.");
+ })
+
+ app.get("/heroes", function(req, res){
+    res.send(heroes);
+ })
+
